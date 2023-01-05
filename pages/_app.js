ec88889823cd5a7a8592c7b30c1dc99074c8e542
RyanPrabowo <ryan.prabowo@omniretail.co>
2023-01-05 13:25:47 +0700

    loader for page transistion and fix padding to header

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
 import '../styles/globals.css'
+import "nprogress/nprogress.css";
+import { Router } from 'next/router';
+import nProgress from 'nprogress';
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
